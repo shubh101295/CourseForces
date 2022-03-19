da7088ac9ea6c6e6b3bde8ad84c8cb769ba8bf8c
shubh101295 <shubhagrawal101295@gmail.com>
2022-03-19 12:19:12 +0530

    Update mail contents

--- backend/quiz_app/quiz_app/email_settings.py
+++ backend/quiz_app/quiz_app/email_settings.py
@@ -7,13 +7,13 @@ EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_TITLE={
-	"ACTIVATION":"Activate your Course Connect password",
-	"FORGOT_PASSWORD":"Change your Course Connect password"
+	"ACTIVATION":"Activate your CourseForces account",
+	"FORGOT_PASSWORD":"Change your CourseForces password"
 }
 
 EMAIL_BASE_LINK = 'http://127.0.0.1:8000/users/'
 
 EMAIL_CONTENT = {
-	"ACTIVATION" : """Hi {name:s},\nActivate your course connect account by clicking on the link below or copy pasting it in the browser\n\n\n\n\n""", 
+	"ACTIVATION" : """Hi {name:s},\nActivate your CourseForces account by clicking on the link below or copy pasting it in the browser\n\n\n\n\n""", 
 	"FORGOT_PASSWORD":"""Hi {name:s},\nClick on the link below or copy paste it in the browser to change the password of your account\n\n\n\n\n"""
 }
