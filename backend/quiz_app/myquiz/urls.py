5bf104bbf7bcde611ae96819fa60bcd2eec83bd7
shubh101295 <shubhagrawal101295@gmail.com>
2022-04-07 02:08:45 +0530

    Add question_delete API

--- backend/quiz_app/myquiz/urls.py
+++ backend/quiz_app/myquiz/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
 
 from .views import (
-    add_quiz,add_question
+    add_quiz,add_question,delete_question
 )
 
 urlpatterns = [
 	path('add/',add_quiz, name="add_quiz"),
 	path('question/add/',add_question, name="add_question"),
+	path('question/delete/',delete_question, name="delete_question"),
+	# path('question/add/',add_question, name="add_question"),
 
 ]
--- backend/quiz_app/myquiz/views.py
+++ backend/quiz_app/myquiz/views.py
@@ -26,9 +26,9 @@ def add_quiz(request):
 			quiz_data["start_at"] = request.data.get("start_at","")
 			serializer = QuizSerializer(data = quiz_data)
 			if serializer.is_valid():				
-				quizes = Quiz.objects.filter(Q(title=quiz_data["title"])) 
+				quizes = Quiz.objects.filter(Q(title=quiz_data["title"]))
 				for temp_quiz in quizes:
-					quiz_in_course_relations = quiz_in_course.objects.filter(Q(course=course.id) & Q(quiz=temp_quiz))
+					quiz_in_course_relations = quiz_in_course.objects.filter(Q(course=course.id) & Q(quiz=temp_quiz)).distinct()
 					# print("YOYO ",quiz_in_course_relations,len(quiz_in_course_relations))
 					if len(quiz_in_course_relations)>0:
 						return Response("Quiz with the same title exists in the course", status=status.HTTP_400_BAD_REQUEST)
@@ -40,7 +40,7 @@ def add_quiz(request):
 					"quiz_pk":quiz_response.pk 
 					} ,status=status.HTTP_200_OK)
 			return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST) 
-		return Response("You are not Prof in the course",status=status.HTTP_401_UNAUTHORIZED)
+		return Response("You are not Professor in the course",status=status.HTTP_401_UNAUTHORIZED)
 	return Response(util_data["error_message"], status=util_data["status"])
 
 @api_view(["POST"])
@@ -52,7 +52,7 @@ def add_question(request):
 		if util_data["relation"] == 'P': 
 			quiz_pk = request.data.get("quiz_pk", "")
 			if quiz_pk!="":
-				quiz_in_course_relations = quiz_in_course.objects.filter(Q(course=course) & Q(quiz=quiz_pk))
+				quiz_in_course_relations = quiz_in_course.objects.filter(Q(course=course) & Q(quiz=quiz_pk)).distinct()
 				if len(quiz_in_course_relations)==1:
 					question_data = {}
 					question_data["content"] = request.data.get("content" ,"")
@@ -64,7 +64,7 @@ def add_question(request):
 					serializer = QuestionSerializer(data = question_data)
 					if serializer.is_valid():				
 						question_response = serializer.save()
-						new_question_in_quiz_relation = question_in_quiz(question=question_response,quiz=quiz_in_course_relations[0].quiz)
+						new_question_in_quiz_relation = question_in_quiz(question=question_response,quiz=quiz_in_course_relations[0].quiz).distinct()
 						new_question_in_quiz_relation.save()
 						return Response({
 							"question_pk":question_response.pk
@@ -72,5 +72,29 @@ def add_question(request):
 					return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
 				return Response("No such quiz found in the course", status=status.HTTP_400_BAD_REQUEST)
 			return Response("quiz_pk should not be empty",status=status.HTTP_400_BAD_REQUEST)
-		return Response("You are not Prof in the course",status=status.HTTP_401_UNAUTHORIZED)
+		return Response("You are not Professor in the course",status=status.HTTP_401_UNAUTHORIZED)
+	return Response(util_data["error_message"], status=util_data["status"])
+
+@api_view(["POST"])
+def delete_question(request):
+	user = getUser(request)
+	util_data,course = user_in_course_details(user,request.data.get("course_pk",""))
+	if util_data["allowed"]:
+		if util_data["relation"] == 'P': 
+			quiz_pk = request.data.get("quiz_pk", "")
+			if quiz_pk!="":
+				quiz_in_course_relations = quiz_in_course.objects.filter(Q(course=course) & Q(quiz=quiz_pk))
+				if len(quiz_in_course_relations)==1:
+					question_pk = request.data.get("question_pk", "")
+					if question_pk!="":
+						question_in_quiz_relations = question_in_quiz.objects.filter(Q(question=question_pk) & Q(quiz=quiz_pk))
+						if len(question_in_quiz_relations)==1:
+							question_relations = Question.objects.filter(Q(pk=question_pk))
+							question_relations.delete()
+							return Response("Succesfully deleted the question",status=status.HTTP_200_OK)
+						return Response("No such question found in the quiz", status=status.HTTP_400_BAD_REQUEST)
+					return Response("question_pk should not be empty",status=status.HTTP_400_BAD_REQUEST)
+				return Response("No such quiz found in the course", status=status.HTTP_400_BAD_REQUEST)
+			return Response("quiz_pk should not be empty",status=status.HTTP_400_BAD_REQUEST)
+		return Response("You are not Professor in the course",status=status.HTTP_401_UNAUTHORIZED)
 	return Response(util_data["error_message"], status=util_data["status"])
